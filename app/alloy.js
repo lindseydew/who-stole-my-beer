ccc01d70b125c79aed016977e2ac1fce76c130e3
Lindsey Dew <lindsey.dew@guardian.co.uk>
2013-02-03 16:09:47 +0000

    add the alloy.js file back in

--- app/alloy.js
+++ app/alloy.js
@@ -12,3 +12,19 @@
 
 
 // variables here etc all available in all controllers/
+var criminalPerson;			// The guilty person
+var suspectsList; 			// people collection randomly chosen from available suspects
+var suspectCount;			// An incremented value changed each level to find out how many people to get		
+var remainingGameTime;		// How long is left before timeout
+var totalTimeThisLevel; 	// How many seconds will this level last for
+var currentLevel;			// Current level of gameplay
+var currentSuspectCount;  	//How many suspects are displayed for this level
+var gamerName;				// Name of the gamer for the leaderboard
+var timerInterval;			// How long between ticks
+var lastTickTime;			// The last time that a tick occurred (used to work out when a tick has occured)
+var levelTimeReduction; 	// constant - allows time to be reduced by a fixed amount till it reaches a minimum time
+var leaderboard ;			// A collection of previous game stats
+
+var initial_suspect_count=6; // How many suspects are got at the start of the game
+var max_pass_images=1; 		// How many pictures currently exist in the pass folder (means we can add more later)
+var max_fail_images=1; 		// How many pictures currently exist in the pass folder (means we can add more later)
